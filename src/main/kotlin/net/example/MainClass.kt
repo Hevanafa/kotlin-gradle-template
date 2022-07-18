0a2e73af9e69ed4b8818ed2ac33d5396413c797b
Hevanafa <dhevansafa4@gmail.com>
2022-07-18 11:33:27 +0700

    adding example class with main constructor

--- src/main/kotlin/net/example/MainClass.kt
+++ src/main/kotlin/net/example/MainClass.kt
@@ -1,9 +1,18 @@
 package net.example
 
+private class SubClass {
+    init {
+        println("SubClass constructor.")
+    }
+}
+
+// driver class
 class MainClass {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             println("Hello from Kotlin!")
+
+            SubClass()
         }
     }
 }
